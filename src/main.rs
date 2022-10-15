106b151f0ea84d6243b69fbd4b3204f21a316f45
jonathan <jonathantbrouwer@gmail.com>
2022-10-15 17:14:34 +0200

    Verify chains

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@
 
 use ndarray::Array2;
 use std::io::{stdin, BufRead};
+use std::time::Instant;
 
 fn main() {
     let mut words: Vec<&str> = include_str!("../resources/words.txt").split("\n").collect();
-    words.sort();
 
     let stdin = stdin();
     let lock = stdin.lock();
@@ -24,34 +24,41 @@ fn main() {
         }
     }
 
-    let testwords = ["mint", "mite"];
-
+    let t_before = Instant::now();
     let result = eval_grid((h, w), &grid, &words[..]);
-    println!("{:?}", result)
+    println!("{:?}", result);
+    println!("Took: {:?}", t_before.elapsed());
 }
 
 fn eval_grid<'a>(dims: (usize, usize), grid: &Array2<u8>, words: &[&'a str]) -> Vec<&'a str> {
     let mut result: Vec<&'a str> = Vec::new();
 
-    let mut stack: Vec<(u8, Vec<(usize, usize)>)> = Vec::with_capacity(dims.0 * dims.1);
 
-    'wl: for word in words {
+
+    'wl: for (i, word) in words.iter().enumerate() {
+        let mut stack: Vec<(u8, Vec<(usize, (usize, usize))>)> = Vec::with_capacity(dims.0 * dims.1);
         // First, pop what isn't needed
-        while let Some((last, _)) = stack.last() {
-            if word.len() < stack.len() && *last == word.as_bytes()[stack.len() - 1] {
-                break;
-            }
-            stack.pop().unwrap();
-        }
+        // while let Some((last, _)) = stack.last() {
+        //     if word.len() < stack.len() && *last == word.as_bytes()[stack.len() - 1] {
+        //         break;
+        //     }
+        //     stack.pop().unwrap();
+        // }
 
-        if let Some((_, last)) = stack.last() && last.is_empty() { continue; }
+        // if let Some((_, last)) = stack.last() && last.is_empty() { continue; }
 
         // Then, push what is needed
         for b in &word.as_bytes()[stack.len()..] {
-            let next: Vec<(usize, usize)> = if let Some((_, last)) = stack.last() {
-                last.iter().map(|p| neighbours(dims, *p)).flatten().filter(|p| grid[*p] == *b).collect()
+            let next: Vec<(usize, (usize, usize))> = if let Some((_, last)) = stack.last() {
+                last.iter()
+                    .enumerate()
+                    .map(|(i, (_, p))| neighbours(dims, *p).map(move |ps| (i, ps)))
+                    .flatten()
+                    .filter(|(_, p)| grid[*p] == *b)
+                    .filter(|(i, e)| verify_chain(&stack[..], *i, *e))
+                    .collect()
             } else {
-                all_cells(dims).filter(|p| grid[*p] == *b).collect()
+                all_cells(dims).filter(|p| grid[*p] == *b).map(|p| (0, p)).collect()
             };
             let should_stop = next.is_empty();
             stack.push((*b, next));
@@ -61,11 +68,25 @@ fn eval_grid<'a>(dims: (usize, usize), grid: &Array2<u8>, words: &[&'a str]) -> 
         }
 
         //We finished the word!
-        result.push(word)
+        result.push(word);
     }
 
 
     result
+}
+
+fn verify_chain<T: Eq>(words: &[(u8, Vec<(usize, T)>)], i: usize, e: T) -> bool {
+    match words.split_last() {
+        None => true,
+        Some((next, rest)) => {
+            let next = &next.1[i];
+            if next.1 == e {
+                false
+            } else {
+                verify_chain(rest, next.0, e)
+            }
+        }
+    }
 }
 
 fn all_cells(dims: (usize, usize)) -> impl Iterator<Item = (usize, usize)> {
